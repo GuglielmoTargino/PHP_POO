e9e99bc0e96f038623a82f00b535ceb310fe622c
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-07-20 11:21:18 -0300

    mod_form
    Modelo de formulario

---
+++ CAP3_MANIP_DADOS/modelo_formulario.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+
+<?php
+/*Exrcicio modelo de formulario tirado da internet
+Aluno: Guglielmo Targino
+Data:20jul24
+Versão: v0
+
+ */
+  ?>
+    <form name="form1" method="post" action="php_poo-DAO_Oracle_OCI.php">
+    <label> User Name
+      <input type="text" name="nis" id="nis">
+      </label>
+      <label> Password
+      <input type="password" name="password" id="password">
+      </label>
+      <label>
+      <input type="submit" name="submit" id="button" value="Login">
+      </label>
+  </form>
+
+  
+
+ 
+
+  </html>
+
--- CAP3_MANIP_DADOS/mysql_insert.php
+++ CAP3_MANIP_DADOS/mysql_insert.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+<meta charset="utf-8">
+<meta lang="pt-br">
 
 <?php
 
