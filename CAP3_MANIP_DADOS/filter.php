01562fe807443ac2111a35fb9666c1137650065f
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-07-22 19:49:32 -0300

    test_ok
    Teste feito ok

--- CAP3_MANIP_DADOS/filter.php
+++ CAP3_MANIP_DADOS/filter.php
@@ -9,15 +9,15 @@
 
 <?php
 //Carrega classes
-include_once 'app.ado/php_poo-TExpression.php';
-include_once 'app.ado/php_poo-TFilter.php';
+include_once 'app.ado/php_poo-TExpression.class.php';
+include_once 'app.ado/php_poo-TFilter.class.php';
 
 // Cria filtros por data (string)
 $filter1=new TFilter('data','=','2024-03-01');
 echo $filter1->Dump();
 echo"<br>\n";
 
-/*
+
 
 //Cria filtro por salario(integer)
 $filter2=new TFilter('salario','>',30000);
@@ -38,6 +38,6 @@ echo"<br>\n";
 $filter5=new TFilter('ativo','=',true);
 echo $filter5->Dump();
 echo"<br>\n";
- */
+ 
 ?>
 </html>
