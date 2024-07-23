ac8c29fc3e2b9d26414052b098599de5b1b91bad
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-07-23 20:05:19 -0300

    fim_delete
    Finalizada a classe delete

--- CAP3_MANIP_DADOS/app.ado/TSqldelete.class.php
+++ CAP3_MANIP_DADOS/app.ado/TSqldelete.class.php
@@ -7,8 +7,20 @@
  * Versão:v0.
  * pgn 193
  */
+final class TSqlDelete extends TSqlInstruction{
+    public function getInstruction(){
+        //monta string de delete
+        $this->sql="DELETE FROM {this->entity}";
+        //retorna clausula where
+        if($this->criteria){
+            $expression=$this->criteria->dump();
+            if($expression){
+                $this->sql.='WHERE'.$expression;
+            }
 
-
-
+        }
+        return $this->sql;
+    }
+}
 
 ?>
