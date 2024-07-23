6b238c7e3bc3ab203447f00b2895a2399e8ae2df
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-07-23 17:16:34 -0300

    fim_cgo_update
    Finalizado o codigo principal update

--- CAP3_MANIP_DADOS/update.php
+++ CAP3_MANIP_DADOS/update.php
@@ -8,22 +8,49 @@
 
 <?php
 
-/**
+/*
  * Exercício d livro PHP-POO.
  * Aluno: Guglielmo Targino.
  * Data: 23jul24.
  * Versão:v0
  * pgn 191.
- */
-
- //carrega função autoload();
-
- function __autoload($cla){
+ * function __autoload($cla){
     if(file_exists("app.ado/{$cla}.class.php")){
         include_once "app.ado/{$cla}.class.php";
     }
+ */
 
- }
+ //carrega função autoload();
+   
+include_once 'app.ado/php_poo-TSqlInstruction.class.php'; 
+include_once 'app.ado/php_poo-TExpression.class.php';
+include_once 'app.ado/php_poo-Tcriteria.class.php';
+include_once 'app.ado/php_poo-TFilter.class.php';
+include_once 'app.ado/php_poo-TSqlupdate.class.php';
+
+
+    
+ 
+ 
+ //cria criterio de seleção.
+ $criterio=new Tcriteria;
+ $criterio->add(new TFilter('id','=','3'));
+
+ //cria instruction de update
+ $sql=new TSqlUpdate;
+ //define a entrada;
+ $sql->setEntity('aluno');
+ //atribui o valor de cada coluna;
+ $sql->setRowData('nome','dara');
+ $sql->setRowData('rua','concordancia');
+ $sql->setRowData('fone','11-30-30');
+ //define criterio de seleção
+ $sql->setCriteria($criterio);
+
+ // exibe comando sql
+ echo $sql->getInstruction();
+ echo "<br>\n";
+
 
 ?>
 </html>
