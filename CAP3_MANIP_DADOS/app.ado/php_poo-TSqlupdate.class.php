08ec13def656a0ae9ee9101e6732455f2c1e98e8
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-07-23 16:32:20 -0300

    fim_class_update
    Finalizada  a criação da classe update

--- CAP3_MANIP_DADOS/app.ado/php_poo-TSqlupdate.class.php
+++ CAP3_MANIP_DADOS/app.ado/php_poo-TSqlupdate.class.php
@@ -6,8 +6,54 @@
  * Versão: v1
  * pgn 189.
  */
+final class TSqlUpdate extends TSqlInstruction{
 
- 
+    private $columnvalueS;
+
+    public function setRowData($column, $value){
+        //monta array idexada pelo valor da coluna
+        if(is_string($value)){
+            //aplica aspas
+            $value=addslashes($value);
+            //se for string
+            $this->columnvalueS[$column]="'$value'";
+        } else if(is_bool($value)){
+            $this->columnvalueS[$column]=$value?'TRUE':'FALSE';
+        }else if(isset($value)){
+            //se for um dado
+            $this->columnvalueS[$value]=$value;            
+        } else{
+            //se for null
+            $this->columnvalueS="NULL";
+        }
+    }
+    public function getInstruction(){
+        //mostra a string de update
+        $this->sql="UPDATE{$this->entity}";
+        
+        //monta os pares coluna=valor
+        if($this->columnvalueS){
+            foreach($this->columnvalueS as $column=>$value){
+                $set[]="{$column}={$value}";
+            }
+        }
+        $this->sql.='SET'.implode(',',$set);
+
+        //retorna a clausula where do objeto $this->criteria;
+
+        if($this->criteria){
+            $this->sql.='WHERE'.$this->criteria->dump();
+        }
+
+        return $this->sql;
+
+
+    }
+    
+        
+    
+}
+
 
 
 ?>
