e465c10a7e13de34e7481084f14c9ab5b0bb7ddd
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-07-24 11:01:01 -0300

    fim_cdprin_select
    Finalizado o codigo principal select

--- CAP3_MANIP_DADOS/select.php
+++ CAP3_MANIP_DADOS/select.php
@@ -12,12 +12,36 @@
  * Versão:v0
  * pgn 197
  */
+    //carrega as classes necessárias automaticamente 
+    //no momento em que são instaciadas pelos objetos.
+
 
  //inicia criterio de seleção
  $criteria=new Tcriteria;
  $criteria->add(new TFilter('nome','like','maria%'));
  $criteria->add(new TFilter('cidade','like','porto%'));
- 
+
+ //define o intervalo da consulta.
+ $criteria->setProperty('offset',0);
+ $criteria->setProperty('limit',10);
+ //ordenação da consulta
+ $criteria->setProperty('order','nome');
+
+ //cria seleção
+ $sql=new TSqlSelect;
+ //define entidade/tabela
+ $sql->setEntity('aluno');
+ //adiciona coluna
+ $sql->_addColumn('nome');
+ //define criterio de seleção
+ $sql->setCriteria($criteria);
+ //processa instrução
+ echo $sql->getInstruction();
+ echo"<br>\n";
+
+
+
+
 
 ?>
 </html>
