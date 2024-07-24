9c130e9a3b5a3bab1114570a150cb3cd863cdb9c
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-07-24 11:53:36 -0300

    ini_html_php
    Iniciada a tela para exercicios em php

--- CAP3_MANIP_DADOS/select.php
+++ CAP3_MANIP_DADOS/select.php
@@ -3,7 +3,21 @@
 <head>
 <meta charset="utf-8">
 <title>Modelo inicial para teste em PHP.</title>
+<link rel="stylesheet" type="text/css" href="estilo/estilo_avulso.css" media="screen" />
 </head>
+
+<body>
+    <div id="top">
+        <h1>Programação PHP-POO.</h1>
+    </div>
+</body>
+
+<footer>
+    <h2> Estudar sempre é o melhor caminho!</h2>
+</footer>
+
+
+
 <?php
 /**
  * Exercício do livro PHP-POO
@@ -14,7 +28,11 @@
  */
     //carrega as classes necessárias automaticamente 
     //no momento em que são instaciadas pelos objetos.
-
+    function my_autoloader($cla) {
+        include_once 'app.ado/' . $cla . '.class.php';
+     }
+     
+     spl_autoload_register('my_autoloader');
 
  //inicia criterio de seleção
  $criteria=new Tcriteria;
@@ -38,10 +56,8 @@
  //processa instrução
  echo $sql->getInstruction();
  echo"<br>\n";
-
-
-
-
-
 ?>
+
+
+
 </html>
