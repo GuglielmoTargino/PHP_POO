5171bfd880afa49822e3c3dd0fafbe4b41fa349e
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-07-25 18:23:40 -0300

    ini_class_ttransaction
    Iniciada a classe ttransaction

---
+++ CAP3_MANIP_DADOS/app.ado/ttransaction.class.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Exercício do livro PHP-POO
+ * Aluno: Guglielmo Targino.
+ * Data: 25jul24
+ * Versão:v0
+ * pgn 208
+ */
+
+
+?>
---
+++ CAP3_MANIP_DADOS/transaction.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title>TESTES PHP-POO.</title>
+<link rel="stylesheet" type="text/css" href="estilo/estilo_avulso.css" media="screen" />
+</head>
+
+<body>
+    <div id="top">
+    <h1>CAPÍTULO 3</h1>
+        <h1>PHP-Programando com Orientação a Objetos</h1>
+        <h2>| Manipulação de dados xx</h2>
+<?php
+
+/**
+ * Exercício do livro <PHP-POO
+ * Aluno: Guglielmo Targino
+ * Data:25jul24.
+ * Versão: v0.
+ * pgn 210
+ */
+
+
+?>
+
+</div>
+</body>
+
+</html>
