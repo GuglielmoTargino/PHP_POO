4396a0c37dbe676f9a3c264ac6dc792d859937f1
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-07-24 23:01:50 -0300

    ini_class_bd
    Iniciada a classe de conexão bd

---
+++ CAP3_MANIP_DADOS/app.ado/tconnection.class.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Exercícios do livro PHP-POO
+ * Aluno: Guglielmo Targino.
+ * Data:24jul24
+ * Versão: v0.
+ * pgn202
+ */
+
+ final class TConnection{
+    private function __construct(){
+    }
+    //método open
+    public static function _open($xx){
+        //teste se existe arquivo de conexão.
+        if(file_exists("APP.CONFIG/{$xx}.ini")){
+            //lê arquivo ini
+            $db=parse_ini_file("APP.CONFIG/{$xx}.ini");
+        }else{
+            //se falta dispara erro.
+            throw new Exception("Arquivo não existe");
+        }
+
+        //lê as informações contidas no arquivo
+        $user=$db['user'];
+        $pass=$db['pass'];
+        $name=$db['name'];
+        $host=$db['host'];
+        $type=$db['type'];
+
+        //verifica qual drive do banco de dados a ser utilizado
+        switch($type){
+            case 'pgsq':
+                echo " Banco de dados Postgre!"
+        }
+
+
+    }
+ }
+
+
+
+?>
