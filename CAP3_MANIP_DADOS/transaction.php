6b7091e2667edee91d1f7f9db819df2d9e395b39
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-07-29 21:02:49 -0300

    alfa_cdprim_transation
    Iniciado o codigo principal do programa transaction

--- CAP3_MANIP_DADOS/transaction.php
+++ CAP3_MANIP_DADOS/transaction.php
@@ -21,6 +21,49 @@
  * pgn 210
  */
 
+ try{
+    // abre a conexão
+    TTranssaction::open('pg_livro');
+    //cria instrução insert
+    $sql=new TSqlInsert;
+    // define a o nome da entidade
+    $sql->setEntity('famosos');
+    //atribui valor da coluna
+    $sql->setRowData('codigo',8);
+    $sql->setRowData('nome','galileu');
+
+    // obtem conexão
+    $conn=TTranssaction::get();
+    //executa conexão
+    $result=$conn->Query($sql->getInstruction());
+
+    //cria uma instrução UPDATE
+    $sql=new TSqlUpdate;
+    //define o nome da entidade;
+    $sql->setEntity('famosos');
+    //atribui valor da coluna;
+    $sql->setRowData('nome','galileu');
+    //cria criterio de seleção
+    $criteria=new Tcriteria;
+    //obtem a pessoa de codigo 8;
+    $criteria->add(new TFilter('codigo','=','8'));
+    //atribui o criterio 
+    $sql->setCriteria($criteria);
+    //obtem conexão
+    $conn=TTranssaction::get();
+    //executa a instrução
+    $result=$conn->Query($sql->getInstruction());
+    //fecha a conexão;
+    TTranssaction::close();
+
+ }catch(Exception $e){
+    echo $e->getMessage();
+    //desfaz as operações realzadas durante a transação.
+    TTranssaction::rollback();
+ }
+
+ 
+
 
 ?>
 
