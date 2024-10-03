ba2a26dbc530213a393e1ea095c36100c6b2b633
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-10-03 14:03:56 -0300

    omega_atv3_prog_web
    Finalizada a atividade 3 da aula de programação web.

--- PROTOT/ATV3PROG_WEB/dados.php
+++ PROTOT/ATV3PROG_WEB/dados.php
@@ -1,40 +1,43 @@
 <?php
+/*
+Atividade III - salvando dados de formulário e agradecendo ( vale nota 0 a 10).
+Crie um formulário completo com nome, endereço, telefone, nascimento, sexo, escolaridade com botão SUBMIT e RESET.
+Ao enviar pelo SUBMIT o arquivo PHP salva em um arquivo loguer.txt e envia de volta uma pagina de agradecimento.
+Esta pagina de agradecimento deverá estar salva em arquivo HTML.
+O PHP lê este arquivo e envia ( echo) para o cliente que enviou o formulário.
+
+Prof: João Vagner Pereira.
+Aluno: Guglielmo Henriques Targino.
+RA:2222104623.
+Data: 03out24.
+Versão: v2.
+Parte 2/3.
+ */
         $nome = $_GET['nome'];
         $end = $_GET['end'];
         $tel = $_GET['tel'];
         $data = $_GET['data'];
         $sexo = $_GET['sexo'];
         $escolaridade = $_GET['escolaridade'];
-        $operacao = $_GET['ler'];
-
-        //echo"$nome, $end, $tel, $data, $sexo";
+        $operacao = $_GET['cad'];
 
         if ($operacao == 'Gravar'){
-            $myfile = fopen("C:/Users/Guglielmo H T/Desktoplogue.txt", "w") or die("Unable to open file!");
-            fwrite($myfile, $nome);
-            fwrite($myfile, "<br>");
-            fwrite($myfile, $end);
-            fwrite($myfile, "<br>");
-            fwrite($myfile, $tel);
-            fwrite($myfile, "<br>");
-            fwrite($myfile, $data);
-            fwrite($myfile, "<br>");
-            fwrite($myfile, $sexo);
-            fwrite($myfile, "<br>");
-            fwrite($myfile, $escolaridade);
-            fwrite($myfile, "<br>");
-            fclose($myfile);
 
-   
-            //$myfile = fopen("C:/Users/WS-Edson/Desktop/carta.txt", "r") or die("Unable to open file!");
-            //echo fgets($myfile);
-            //echo $leitura;
-            //fclose($myfile);
-            
-            include('agradecimento.html');
-           // < href="agradecimento.html">;
+            //cria o arquivo logue.txt em branco
+            $myfile = fopen("C:/Users/Guglielmo H T/Desktop/logue.txt", "w") or die("Unable to open file!");
 
-    }
+            //Escreve no arquivo logue.txt os dados informados no formulário pelo modo append. 
+            $myfile = fopen("C:/Users/Guglielmo H T/Desktop/logue.txt", "a") or die("Unable to open file!");
+                fwrite($myfile,"Nome: ".$nome."\n");
+                fwrite($myfile,"Endereço: ".$end."\n");
+                fwrite($myfile,"Telefone: ".$tel."\n");
+                fwrite($myfile,"Data_nasc: ".$data."\n");
+                fwrite($myfile,"Sexo: ".$sexo."\n");
+                fwrite($myfile,"Escolaridade: ".$escolaridade."\n");
+            }
+
+    include('agradecimento.html'); // Requisita página agradecimento.html para nota de agradeciemento.
+?>
 
 
 
