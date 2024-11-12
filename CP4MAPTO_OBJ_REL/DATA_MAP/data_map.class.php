9c9ae58b1d3e438a6c11b01709703e622270f6d8
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-11-12 16:03:44 -0300

    ini_data_map
    Iniciado o exercício com o data mapper

---
+++ CP4MAPTO_OBJ_REL/DATA_MAP/data_map.class.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title>Modelo inicial para teste em PHP.</title>
+</head>
+
+<body>
+<?php
+
+  /**
+   * Exercício do livro PHP-POO
+   * Aluno: Guglielmo Targino
+   * Data: 14ago24
+   * Versão: v0
+   * pgn 247
+   */
+include_once '../DAO_CP4/dao_cp4.php';
+
+
+class ProdutoGateway2{ 
+   private $data;
+
+   function __get($prop)
+
+      {
+         return $this->data[$prop];
+      }
+
+   function __set($prop, $value)
+      {
+         $this->data[$prop]=$value;
+      }
+
+   function Insert(){
+         $conn=ConexaoBd();
+         //cria instrução SQL
+         $sql="INSERT INTO pets (id, nome, tipo, peso)".
+         "VALUES ('{$this->id}','{$this->nome}','{$this->tipo}','{$this->peso}')";
+
+         //executa instrucao SQL
+         $conn->exec($sql);
+         unset($conn);
+    }
+
+
+
+   function Update(){
+      $conn=ConexaoBd();
+      //cria instrução SQL
+      $sql="UPDATE pets set nome='{$this->nome}', tipo='{$this->tipo}', peso='{$this->peso}' where id={$this->id}";
+       //executa instrucao SQL
+       $conn->exec($sql);
+       unset($conn);
+    } 
+
+   function Delete($id){
+         $conn=ConexaoBd();
+         //cria instrução SQL
+         $sql="DELETE FROM pets WHERE id=$id";
+
+       //executa instrucao SQL
+         $conn->exec($sql);
+         unset($conn);
+    }
+
+   function getObject($id){
+         $conn=ConexaoBd();
+         //cria instrução SQL
+         $sql="SELECT* FROM pets WHERE id='$id'";
+
+       //executa instrucao SQL
+         $resu=$conn->query($sql);
+         $data=$resu->fetch(PDO::FETCH_ASSOC);
+         unset($conn);
+         echo "Nome: " . $data['nome'] . "<br>";
+       //print_r($data). "<br>";
+         return $data;
+    }
+
+   function getObjects(){
+         $conn=ConexaoBd();
+         //cria instrução SQL
+         $sql="SELECT* FROM pets";
+
+       //executa instrucao SQL
+         $resu=$conn->query($sql);
+         $data=$resu->fetchAll (PDO::FETCH_ASSOC);
+         foreach ($data as $row) {
+            print_r($row);
+            echo "<br>"; // Pula uma linha entre cada array para maior clareza         
+         }     
+         /*  foreach ($data as $row) {
+         echo "<div>";
+         foreach ($row as $key => $value) {
+             echo "<strong>$key:</strong> $value<br>";
+         }
+         echo "</div><hr>"; // Linha horizontal entre cada array
+         }  
+         */  
+
+    }
+    
+}
+   
+?>
+</body>
+
+</html>
---
+++ CP4MAPTO_OBJ_REL/DATA_MAP/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--Exrcicio modelo de formulario
+Aluno: Guglielmo Targino
+Data:09nov24
+Versão: v3
+pg254-257
+-->
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title>Modelo inicial para teste em PHP.</title>
+<link rel="stylesheet" type="text/css" href="../../STYLE/estilo_avulso.css" media="screen" />
+</head>
+
+
+
+<?php
+include_once 'row_gateway.class.php';
+
+function testa1(ProdutoGateway2 $objeto){   
+    $objeto->Insert(); //INSERIR 
+}   
+
+function testa2(ProdutoGateway2 $objeto,$x){
+    $objeto->Update($x);//ATUALIZAR
+}
+
+function testa3(ProdutoGateway2 $objeto,$x){   
+    $objeto->getObject($x); //SELECT
+}
+function testa4(ProdutoGateway2 $objeto,$x){   
+    $objeto->getObjects(); //SELECT ALL
+}
+function testa5(ProdutoGateway2 $objeto,$x){   
+    $objeto->Delete($x); //DELETE
+}
+
+$objeto= new ProdutoGateway2;
+
+$objeto->id=99;
+$objeto->nome='mina';
+$objeto->tipo='gata';
+$objeto->peso=50;
+
+testa4($objeto,$objeto->id);   
+    
+?>
+
+</html>
+
